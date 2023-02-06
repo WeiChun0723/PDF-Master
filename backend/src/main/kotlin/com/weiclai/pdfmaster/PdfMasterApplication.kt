83ad6767d34619de4883a3d9d917b171351d07fa
WeiChun0723 <laiwc0723@gmail.com>
2023-02-06 17:50:46 +0800

    commit unresolved files and update development log

---
+++ backend/src/main/kotlin/com/weiclai/pdfmaster/PdfMasterApplication.kt
@@ -0,0 +1,11 @@
+package com.weiclai.pdfmaster
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PdfMasterApplication
+
+fun main(args: Array<String>) {
+	runApplication<PdfMasterApplication>(*args)
+}
