6d8343671e816da14c4bf85799eb77a10d73e42c
WeiChun0723 <laiwc0723@gmail.com>
2026-03-01 15:11:58 +0800

    remove unnecessary doc

--- backend/src/main/kotlin/com/weiclai/pdfmaster/PdfMasterApplication.kt
+++ backend/src/main/kotlin/com/weiclai/pdfmaster/PdfMasterApplication.kt
@@ -2,9 +2,24 @@ package com.weiclai.pdfmaster
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class PdfMasterApplication
+class PdfMasterApplication {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer =
+        object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry
+                    .addMapping("/**")
+                    .allowedOriginPatterns("https://pdf-master-*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+            }
+        }
+}
 
 fun main(args: Array<String>) {
     runApplication<PdfMasterApplication>(*args)
